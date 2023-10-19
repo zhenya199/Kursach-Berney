f15d4d98b2e237f38f41c39f2198405eaf7d83aa
zhenya199 <148492358+zhenya199@users.noreply.github.com>
2023-10-19 22:26:40 +0300

    Kursach

---
+++ Проект познавательное приложение Угадай мелодию/Unit6.pas
@@ -0,0 +1,40 @@
+unit Unit6;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.jpeg,
+  Vcl.StdCtrls, Vcl.Imaging.pngimage;
+
+type
+  TForm6 = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+    Label1: TLabel;
+    Timer1: TTimer;
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form6: TForm6;
+
+implementation
+
+{$R *.dfm}
+
+uses Unit1, Unit2, Unit4, Unit5;
+
+
+
+
+procedure TForm6.Timer1Timer(Sender: TObject);
+begin
+close;
+end;
+
+end.
